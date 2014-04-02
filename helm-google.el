28d46b3663b0621edc598850d22ddefdc6d0df30
steckerhalter <steckerhalter@postmomentum.ch>
2014-04-03 00:24:23 +0200

    try to scrape the website

--- helm-google.el
+++ helm-google.el
@@ -29,31 +29,72 @@
 ;;; Code:
 
 (require 'helm)
-(require 'google)
+
+(defgroup helm-google '()
+  "Customization group for `helm-google'."
+  :link '(url-link "http://github.com/steckerhalter/helm-google")
+  :group 'convenience
+  :group 'comm)
+
+(defcustom helm-google-tld "com"
+  "The TLD of the google url to be used (com, de, fr, co.uk etc.)."
+  :type 'string
+  :group 'helm-google)
 
 (defvar helm-google-input-history nil)
 
+(defun helm-google-url () "URL to google searches."
+       (concat "https://www.google." helm-google-tld "/search?ion=1&q=%s"))
+
+(defun helm-google--process-html (html)
+  (replace-regexp-in-string
+   "\n" ""
+   (with-temp-buffer
+     (insert html)
+     (html2text)
+     (buffer-substring-no-properties (point-min) (point-max)))))
+
+(defun helm-google--parse (buf)
+  "Extract the search results from BUF."
+  (with-current-buffer buf
+    (setq case-fold-search nil)
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "charset=utf-8" nil t)
+        (set-buffer-multibyte t)))
+    (goto-char url-http-end-of-headers)
+    (prog1 (let (results result)
+             (while (re-search-forward "class=\"r\"><a href=\"/url\\?q=\\(.*?\\)&amp;" nil t)
+               (setq result (plist-put result :url (match-string-no-properties 1)))
+               (re-search-forward "<b>\\(.*?\\)</b>" nil t)
+               (setq result (plist-put result :title (match-string-no-properties 1)))
+               (re-search-forward "class=\"st\">\\([\0-\377[:nonascii:]]*?\\)</span>" nil t)
+               (setq result (plist-put result :content (helm-google--process-html (match-string-no-properties 1))))
+               (add-to-list 'results result)
+               (setq result nil))
+             results)
+      (kill-buffer buf))))
+
+(defun helm-google--search (text)
+  (let* ((url (message (format (helm-google-url) (url-hexify-string text))))
+         (buf (url-retrieve-synchronously url t t))
+         (results (helm-google--parse buf)))
+    results))
+
 (defun helm-google-search ()
-  (let* ((results (google-search helm-pattern))
-         (responseData (google-result-field 'responseData results))
-         (records (google-result-field 'results responseData)))
-    (mapcar (lambda (record)
+  (let* ((results (helm-google--search helm-pattern)))
+    (mapcar (lambda (result)
               (concat
                (propertize
-                (google-result-field 'titleNoFormatting record)
+                (plist-get result :title)
                 'face 'font-lock-variable-name-face)
                "\n"
-               (replace-regexp-in-string
-                "\n" ""
-                (with-temp-buffer
-                  (insert (google-result-field 'content record))
-                  (html2text)
-                  (buffer-substring-no-properties (point-min) (point-max))))
+               (plist-get result :content)
                "\n"
                (propertize
-                (url-unhex-string (google-result-field 'url record))
+                (plist-get result :url)
                 'face 'link)))
-            records)))
+            results)))
 
 (defun helm-google-display-to-real (candidate)
   (caddr (split-string candidate "[\n]+")))
